40b3f69c943892737f48a95be0b69b4cb1257b9e
NickVinok <prais.d123@yahoo.com>
2017-10-29 23:27:03 +0300

    Trying to initiate this rep

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+/*
+    <applet code="Main" width=ЗOO height=lOO>
+    </applet>
+*/
+
+public class Main extends Applet implements
+        MouseListener,MouseMotionListener{
+    String msg = "";
+    int mouseX = 0, mouseY = 0;
+
+    public void init(){
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+
+    public void mouseClicked(MouseEvent me){
+        mouseX = 0;
+        mouseY = 10;
+        msg = "Mouse clicked.";
+        repaint();
+    }
+
+    public void mouseEntered(MouseEvent me){
+        mouseX = 0;
+        mouseY = 10;
+        msg = "Mouse entered.";
+        repaint();
+    }
+
+    public void mouseExited(MouseEvent me){
+        mouseX = 0;
+        mouseY = 10;
+        msg = "Mouse exited.";
+        repaint();
+    }
+
+    public void mousePressed(MouseEvent me){
+        mouseX = me.getX();
+        mouseY = me.getY();
+        msg = "Down";
+        repaint();
+        repaint();
+    }
+
+    public void mouseReleased(MouseEvent me){
+        mouseX = me.getX();
+        mouseY = me.getY();
+        msg = "Up";
+        repaint();
+    }
+
+    public void mouseDragged(MouseEvent me){
+        mouseX = me.getX();
+        mouseY = me.getY();
+        msg = "*";
+        showStatus("Dragging mouse at " + me.getX() + ", " + me.getY());
+        repaint();
+    }
+
+    public void mouseMoved(MouseEvent me){
+        showStatus("Moving mouse at " + me.getX() + ", " + me.getY());
+    }
+
+    public void paint(Graphics graphics){
+        graphics.drawString(msg, mouseX, mouseY);
+    }
+}
